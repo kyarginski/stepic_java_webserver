1f1a6ea98f3f5be82c85c8728e6c19bba81fb0f5
Victor Kyarginsky <kyarginski@gmail.com>
2015-12-24 00:09:35 +0300

    HW01. Test passed. Task pass-key: 0a71399f90f079c4dd8a6530c1593aaa

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,11 +1,18 @@
 package main;
 
+import accounts.AccountService;
+import accounts.UserProfile;
+import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.server.handler.ResourceHandler;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 import servlets.AllRequestsServlet;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
+import servlets.SessionsServlet;
+import servlets.UsersServlet;
 
 
 /**
@@ -15,10 +22,14 @@ import org.apache.log4j.Logger;
  *         <p> *
  *         Задача:
  *
- *         Написать сервлет, который будет обрабатывать запросы на /mirror
- *         При получении GET запроса с параметром key=value сервлет должен вернуть в response строку содержащую value.
- *
- *         Например, при GET запросе /mirror?key=hello сервер должен вернуть страницу, на которой есть слово "hello".
+        Написать сервер с двумя сервлетами:
+        SignUpServlet для обработки запросов на signup и
+        SignInServlet для обработки запросов на signin
+
+        Сервлеты должны слушать POST запросы с параметрами
+        login
+        password
+
  */
 public class Main {
 
@@ -29,15 +40,24 @@ public class Main {
 
         log.info("Server started");
 
-        AllRequestsServlet allRequestsServlet = new AllRequestsServlet();
+        AccountService accountService = new AccountService();
+
+        accountService.addNewUser(new UserProfile("admin"));
+        accountService.addNewUser(new UserProfile("test"));
 
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-        context.addServlet(new ServletHolder(allRequestsServlet), "/mirror");
+        context.addServlet(new ServletHolder(new UsersServlet(accountService)), "/api/v1/users");
+        context.addServlet(new ServletHolder(new SessionsServlet(accountService)), "/api/v1/sessions");
+
+        ResourceHandler resource_handler = new ResourceHandler();
+        resource_handler.setResourceBase("public_html");
+
+        HandlerList handlers = new HandlerList();
+        handlers.setHandlers(new Handler[]{resource_handler, context});
 
         Server server = new Server(8080);
-        server.setHandler(context);
+        server.setHandler(handlers);
 
         server.start();
-        server.join();
-    }
+        server.join();    }
 }
